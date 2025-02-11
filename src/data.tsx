91569bf765bf99a40d80c394fd0e252aa97436a9
ChristianDPeralta <christianperalta656@gmail.com>
2025-02-11 15:32:57 +0800

    first commit

---
+++ src/data.tsx
@@ -0,0 +1,74 @@
+export const sculptureList = [{
+    name: 'Starry Night',
+    artist: 'Vincent van Gogh',
+    description: 'A swirling night sky over a quiet town, painted in bold and expressive brushstrokes.',
+    url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/350px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg',
+    alt: 'De sterrennacht'
+  }, {
+    name: 'Mona Lisa',
+    artist: 'Leonardo da Vinci',
+    description: ' A portrait of a woman with a mysterious smile, considered one of the most famous paintings in history.',
+    url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/270px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg',
+    alt: 'La Gioconda'
+  }, {
+    name: 'Solo Leveling ',
+    artist: 'Jang Sung-Rak',
+    description: ' A visually stunning action-fantasy manhwa following Sung Jin-Woo, the weakest hunter who gains the ability to level up infinitely after surviving a deadly dungeon. The series features intense battles, breathtaking artwork, and an overpowered protagonist leading an army of shadows."',
+    url: 'https://upload.wikimedia.org/wikipedia/en/thumb/6/6c/Solo_Leveling_Volume_1_Cover.jpg/220px-Solo_Leveling_Volume_1_Cover.jpg',
+    alt: '나 혼자만 레벨업 (Na Honjaman Level Up)'
+  }, {
+    name: 'Attack on Titan',
+    artist: 'Hajime Isayama',
+    description: 'In a world where humanity is on the brink of extinction due to giant humanoid creatures called Titans, Eren Yeager joins the military to fight back. As the story unfolds, dark secrets about the Titans and the world are revealed, leading to intense battles and shocking twists.',
+    url: 'https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p10701949_b_v8_ah.jpg',
+    alt: '進撃の巨人 (Shingeki no Kyojin)'
+  }, {
+    name: 'Haikyuu!!',
+    artist: 'Haruichi Furudate.',
+    description: 'A high-energy sports anime following Shoyo Hinata, a short but determined volleyball player, as he joins Karasuno High’s team. Alongside his rival-turned-teammate, Tobio Kageyama, he aims to take his team to the top against fierce opponents in thrilling matches.',
+    url: 'https://149367133.v2.pressablecdn.com/wp-content/uploads/2024/08/haikyu-touch-the-dream-mobile-game-southeast-asia-launch-pre-registration-20240806-gadgetmatch.jpg',
+    alt: 'ハイキュー!!'
+  }, {
+    name: 'Nanatsu no Taizai (The Seven Deadly Sins)',
+    artist: 'Nakaba Suzuki.',
+    description: 'Set in a medieval fantasy world, the story follows the Seven Deadly Sins, a group of powerful knights who were disbanded after being falsely accused of treason. Princess Elizabeth seeks their help to save the kingdom from corrupt Holy Knights, leading to an epic adventure filled with powerful battles and deep friendships.',
+    url: 'https://preview.redd.it/sqwmsefoq8c31.jpg?auto=webp&s=71cbbb2d5c6241c49cafab934678bb4afa044175',
+    alt: '七つの大罪 (Nanatsu no Taizai)'
+  }, {
+    name: 'One Punch Man ',
+    artist: 'Yusuke Murata',
+    description: "Saitama, a hero who can defeat any enemy with a single punch, struggles with the boredom of being overwhelmingly powerful. As he searches for a worthy opponent, he encounters cyborg disciple Genos and the mysterious Hero Association, leading to intense battles against monstrous threats.",
+    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiABNbvCkNfI6RAfliCdOq9Zqz0xzcRj4HhA&s',
+    alt: 'ワンパンマン (Wanpanman)'
+  }, {
+    name: 'Fairy Tail ',
+    artist: 'Hiro Mashima',
+    description: "In the magical kingdom of Fiore, young mage Lucy Heartfilia dreams of joining Fairy Tail, a legendary guild known for its powerful and chaotic wizards. After meeting fire-wielding dragon slayer Natsu Dragneel and his partner Happy, she embarks on thrilling adventures filled with friendship, epic battles, and dangerous quests.",
+    url: 'https://upload.wikimedia.org/wikipedia/en/e/e1/FairyTail-Volume_1_Cover.jpg',
+    alt: 'フェアリーテイル (Fearī Teiru)'
+  }, {
+    name: 'Assassination Classroom',
+    artist: 'Yūsei Matsui',
+    description: ' A mysterious, powerful, and seemingly unkillable creature known as Koro-sensei becomes the homeroom teacher of Class 3-E, a group of misfit students tasked with assassinating him before he destroys the Earth. As they train in both combat and academics, they form an unexpected bond with their target.',
+    url: 'https://m.media-amazon.com/images/M/MV5BZjBkYmRiOTAtY2IwOS00ZWU3LTk3YjItNjMzYWZiMDE1MjcyXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
+    alt: '暗殺教室 (Ansatsu Kyoushitsu)'
+  }, {
+    name: 'One Piece',
+    artist: 'Eiichiro Oda',
+    description: 'Monkey D. Luffy, a young pirate with the ability to stretch like rubber after eating a Devil Fruit, sets sail with his crew, the Straw Hat Pirates, to find the legendary treasure known as the One Piece. Along the way, they face powerful enemies, uncover hidden secrets, and chase their dreams on the Grand Line.',
+    url: 'https://m.media-amazon.com/images/M/MV5BMTNjNGU4NTUtYmVjMy00YjRiLTkxMWUtNzZkMDNiYjZhNmViXkEyXkFqcGc@._V1_.jpg',
+    alt: 'ワンピース (Wan Pīsu)'
+  }, {
+    name: 'Jujutsu Kaisen',
+    artist: 'Gege Akutami',
+    description: 'Yuji Itadori, a high school student with extraordinary physical abilities, gets involved in the world of Jujutsu Sorcerers after consuming a powerful cursed object—Sukuna’s finger. Now, with the King of Curses inside him, he trains at the Tokyo Jujutsu High to fight deadly curses and protect humanity.',
+    url: 'https://m.media-amazon.com/images/M/MV5BNmI1MmYxNWQtY2E5NC00ZTlmLWIzZGEtNzM1YmE3NDA5NzhjXkEyXkFqcGc@._V1_.jpg',
+    alt: ' 呪術廻戦 (Jujutsu Kaisen)'
+  }, {
+    name: 'Demon Slayer ',
+    artist: 'Koyoharu Gotouge',
+    description: 'After his family is slaughtered by demons, young Tanjiro Kamado becomes a Demon Slayer to avenge them and find a cure for his sister, Nezuko, who has been turned into a demon. Alongside his comrades, he battles powerful demons and uncovers the dark secrets of their existence.',
+    url: 'https://images.justwatch.com/poster/304099705/s718/demon-slayer-kimetsu-no-yaiba-to-the-swordsmith-village.jpg',
+    alt: '鬼滅の刃 (Kimetsu no Yaiba)'
+  }];
+  
