b4de8cbc515d386a4a49211d32bb73053075f92a
ChristianDPeralta <christianperalta656@gmail.com>
2025-02-11 16:03:08 +0800

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ export default function Gallery() {
   const [index, setIndex] = useState(0);
   const [showMore, setShowMore] = useState(false);
   const hasNext = index < sculptureList.length - 1;
+  const hasPrev = index > 0;
 
   function handleNextClick() {
-    if (hasNext) {
-      setIndex(index + 1);
-    } else {
-      setIndex(0);
-    }
+    setIndex(hasNext ? index + 1 : 0);
+  }
+
+  function handleBackClick() {
+    setIndex(hasPrev ? index - 1 : sculptureList.length - 1);
   }
 
   function handleMoreClick() {
@@ -19,12 +20,14 @@ export default function Gallery() {
   }
 
   let sculpture = sculptureList[index];
+
   return (
     <>
-    <h1>Christian Dean M. Peralta</h1>
-      <button onClick={handleNextClick}>
-        Next
-      </button>
+      <h1>Christian Dean M. Peralta</h1>
+      <div className="button-group">
+        <button onClick={handleBackClick}>Back</button>
+        <button onClick={handleNextClick}>Next</button>
+      </div>
       <h2>
         <i>{sculpture.name} </i>
         by {sculpture.artist}
